bb7869e0874d4937604d705fbe276d59b4271df6
xuebinx <xuebinsng@gmail.com>
2020-12-04 03:03:31 +0800

    Added file upload optional stuffs

--- Prac06/Default.aspx.cs
+++ Prac06/Default.aspx.cs
@@ -23,5 +23,28 @@ namespace Prac06
 
             lblMessage.Text = msg;
         }
+
+        //For file upload
+        //protected void btn_Insert_Click(object sender, EventArgs e)
+        //{
+        //    ProductBLL prodBLL = new ProductBLL();
+        //    string image = "";
+
+        //    if (fileUpload.HasFile == true)
+        //    {
+        //        image = "Images\\" + fileUpload.FileName;
+        //    }
+
+        //    string msg = prodBLL.ProductInsert(tb_ProductName.Text, tb_ProductDesc.Text, tb_UnitPrice.Text, fileUpload.FileName, tb_StockLevel.Text);
+
+        //    if (msg == "Prodict has been Inserted successfully")
+        //    {
+        //        string saveimg = Server.MapPath(" ") + "\\" + image;
+        //        lbl_Result.Text = saveimg.ToString();
+        //        fileUpload.SaveAs(saveimg);
+        //    }
+
+        //    lbl_Result.Text = msg;
+        //}
     }
 }
